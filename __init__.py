4a580b844cde96721f66dd79d7c724a132dff1c4
luismata5656 <luismata@luishmata.com>
2023-01-28 21:36:51 -0700

    configure actions

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 # Import the Images module from pillow
 from PIL import Image
+import sys
 
-img_name = input("Enter file name: ")
-img_ext = input("Enter file extension: ")
+img = sys.argv[1]
+img_name = img.split(".")[0]
 
 sizes = [(1920,1080),
          (1600,900),
          (1280,720),
          (960,540),
          (64,64),]
-im = Image.open(f"{img_name}.{img_ext}")
+im = Image.open(f"{img}")
 
 for size in sizes:
     im.thumbnail(size, Image.ANTIALIAS)
